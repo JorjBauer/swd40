12b2d9b46fbcc0d3605c7635147cd3218a7d20c9
Jorj Bauer <jorj@jorj.org>
2014-10-23 12:51:47 -0400

    updated to send percentage motors, based on new protocol

--- arduino_dalek_sender/arduino_dalek_sender.ino
+++ arduino_dalek_sender/arduino_dalek_sender.ino
@@ -11,6 +11,19 @@ const int rightButtonPin = 4;
 
 #define RF_BROADCASTID 0
 
+#define kLEFT 0
+#define kRIGHT 1
+#define kFORWARD 0
+#define kBACKWARD 1
+
+/* Protocol states initialized to invalid values */
+int fb_state = -1;  // forward/backward state; 0/1
+int p_state = -1;   // motor percentage; 0 to 10
+
+/* Remote motor states (should they be pulsed right now). Again, init'd to invalid values. */
+int lm_state = -9999; // -10 to +10
+int rm_state = -9999; // -10 to +10
+
 void setup()
 {
   Serial.begin(115200);
@@ -27,14 +40,35 @@ void setup()
   digitalWrite(7, LOW); // fake GND for joystick
 }
 
-void loop()
+void SendNewState(char motor, int state)
 {
-  int leftState = 0;
-  int rightState = 0;
+  int fb_want;
   
-  leftState = digitalRead(leftButtonPin);
-  rightState = digitalRead(rightButtonPin);
+  /* Set the remote end cache for f/b and percent */
+  if (state >= 0) {
+    fb_want = kFORWARD;
+  } else {
+    fb_want = kBACKWARD;
+  }
   
+  int pct_want = abs(state);
+  
+  if (fb_want != fb_state) {
+    rf_send(fb_want == kFORWARD ? 'F' : 'B');
+    fb_state = fb_want;
+  }
+  if (pct_want != p_state) {
+    rf_send('0' + pct_want);
+    p_state = pct_want;
+  }
+  
+  /* Set the appropriate motor to those settings */
+  rf_send(motor);
+}
+
+
+int HandleJoystick()
+{
   int lrAnalog = analogRead(A0);
   int udAnalog = analogRead(A1);
   
@@ -52,67 +86,98 @@ void loop()
    * Somewhat down: 650
    * Full down: 797
    */
+
+  /* If the joystick is quiescent, then return. */
+  if (lrAnalog >= 492 && lrAnalog <= 540 &&
+      udAnalog >= 420 && udAnalog <= 521) {
+        return 0;
+  }
+  
+  /* calculate joystick position, in percentages. */
+  float joyX = 0, joyY = 0;
+  if (lrAnalog < 492) {
+    joyX = - (((float)lrAnalog - 174.0) / 492.0);
+    if (joyX < -1) 
+      joyX = -1;
+  } else if (lrAnalog > 540) {
+    joyX = ((float)lrAnalog - 540.0) / (814.0 - 540.0);
+    if (joyX > 1)
+      joyX = 1;
+  }
+  
+  if (udAnalog < 420) {
+    joyY = - (((float)udAnalog - 156.0) / 420.0);
+    if (joyY < -1)
+      joyY = -1;
+  } else if (udAnalog > 521) {
+    joyY = ((float)udAnalog - 521.0) / (797.0 - 521.0);
+    if (joyY > 1)
+      joyY = 1;
+  }
    
-  int sendLR = 0; // -2 through +2 to send fast-left, normal-left, no-LR, normal-right, or fast-right
-  int sendUD = 0; // same for up-through-down
+   /* Determine what the new lm_state and rm_state should be. */
+   float new_leftmotor;
+   float new_rightmotor;
    
-  if (lrAnalog <= 175) {
-    // full left
-    sendLR = -2;
-  } else if (lrAnalog <= 347) {
-     // Somewhat left
-     sendLR = -1;
-   } else if (lrAnalog >= 810) {
-     // full right
-     sendLR = 2;
-   } else if (lrAnalog >= 710) {
-     // somewhat right
-     sendLR = 1;
+    new_leftmotor = joyY;
+    if (joyX > 0) {
+      new_leftmotor *= (1.0 - joyX);
+    }
+     
+    new_rightmotor = joyY;
+    if (joyX < 0) {
+      new_rightmotor *= (1.0 - (abs(joyX)));
+    }
+  
+   /* Compare the new states with the last sent states. If they differ, then send the new states. */
+   int new_lm_state = (int)(10.0 * (new_leftmotor + 0.05));
+   int new_rm_state = (int)(10.0 * (new_rightmotor + 0.05));
+
+   if (lm_state != new_lm_state) {
+     SendNewState('L', new_lm_state);
+     lm_state = new_lm_state;
    }
    
-   if (udAnalog <= 160) {
-     // full up
-     sendUD = 2;
-   } else if (udAnalog <= 300) {
-    // somewhat up
-    sendUD = 1;
-   } else if (udAnalog >= 790) {
-    // full down
-    sendUD = -2;
-   } else if (udAnalog >= 640) {
-    // somewhat down
-    sendUD = -1;
-   }   
+   if (rm_state != new_rm_state) {
+     SendNewState('R', new_rm_state);
+     rm_state = new_rm_state;
+   }
+
+  /* If we get here, the motors aren't quiescent, and we've told the remote end how we want them set; pulse them. */   
+   rf_send('G'); // one pulse
    
-   if (sendLR < 0) {
-     rf_send("L", 1);
-   }
-   if (sendLR > 0) {
-     rf_send("R", 1);
-   }
+   return 1; // we did something
+}
+
+int HandleShoulder()
+{
+  int leftState = digitalRead(leftButtonPin);
+  int rightState = digitalRead(rightButtonPin);
    
-   if (sendUD < 0) {
-     rf_send("B", 1);
-   }
-   if (sendUD > 0) {
-     rf_send("F", 1);
-   }
-   
-   // leftState and rightState are to swing around the head
    if (leftState && rightState) {
      // ignore double-presses.
-     leftState = rightState = 0;
+     return 0;
    }
    
    if (leftState) {
-     rf_send("(", 1);
+     rf_send('(');
+     return 1;
    } else if (rightState) {
-     rf_send(")", 1);
+     rf_send(')');
+     return 1;
    }
    
-   if (sendLR != 0 || sendUD != 0 || leftState || rightState ) {
-     delay(100);
-   }
+   return 0;
+}
+
+void loop()
+{
+  int didSend = HandleJoystick();
+  didSend |= HandleShoulder();
+  
+  if (didSend) {
+    delay(100);
+  }
 }
 
 
@@ -126,4 +191,13 @@ void rf_send(const char *data, int datasize)
   rf12_sendWait(1);
 }
 
+void rf_send(const char data)
+{
+  while (!rf12_canSend()) {
+    rf12_recvDone();
+  }
+
+  rf12_sendStart(2, &data, 1);
+  rf12_sendWait(1);
+}
 
