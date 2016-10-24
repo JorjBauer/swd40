b5b5083f133c85a73e7f9549e96f9d3a0561c8aa
Jorj Bauer <jorj@jorj.org>
2016-10-24 08:18:04 -0400

    converted to serial protocol; added gun stalk motor control

--- arduino_dalek_shouldermotor/arduino_dalek_shouldermotor.ino
+++ arduino_dalek_shouldermotor/arduino_dalek_shouldermotor.ino
@@ -2,25 +2,22 @@
 
 DualMC33926MotorShield md;
 
-#define shoulderLeftPin 5
-#define shoulderRightPin 6
-
-#define FLOAT_TIME 10
+#define FLOAT_TIME 100
 
 // M1 is connected to both motors for the shoulder
 #define SHOULDER_TURN_SPEED 180
+// M2 is connected to the motor for the gun stalk (or will be, some day, maybe)
+#define GUN_SPEED 90
 
 unsigned long ShoulderTimer = 0;
+unsigned long GunTimer = 0;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600); // intentionally slow to minimize errors
   Serial.println("Initializing secondary motor shield...");
   md.init();
   Serial.println("Initialized.");
-  
-  pinMode(shoulderLeftPin, INPUT);
-  pinMode(shoulderRightPin, INPUT);
 }
 
 void SetTimer(unsigned long *t)
@@ -36,18 +33,35 @@ void loop()
     ShoulderTimer = 0;
     md.setM1Speed(0);
   }
-  
-  if (digitalRead(shoulderLeftPin) == HIGH) {
-    SetTimer(&ShoulderTimer);
-    md.setM1Speed(-SHOULDER_TURN_SPEED);
-    didSomething = 1;
-    Serial.println("L");
+  if (GunTimer && GunTimer < millis()) {
+    GunTimer = 0;
+    md.setM2Speed(0);
   }
-  else if (digitalRead(shoulderRightPin) == HIGH) {
-    SetTimer(&ShoulderTimer);
-    md.setM1Speed(SHOULDER_TURN_SPEED);
-    didSomething = 1;
-    Serial.println("R");
+
+  if (Serial.available()) {
+    uint8_t c = Serial.read();
+    switch (c) {
+      case 'L':
+        SetTimer(&ShoulderTimer);
+        md.setM1Speed(-SHOULDER_TURN_SPEED);
+        didSomething = 1;
+        break;
+      case 'R':
+        SetTimer(&ShoulderTimer);
+        md.setM1Speed(SHOULDER_TURN_SPEED);
+        didSomething = 1;
+        break;
+      case '+':
+        SetTimer(&GunTimer);
+        md.setM2Speed(GUN_SPEED);
+        didSomething = 1;
+        break;
+      case '-':
+        SetTimer(&GunTimer);
+        md.setM2Speed(GUN_SPEED);
+        didSomething = -1;
+        break;
+    }
   }
   
   if (didSomething)
